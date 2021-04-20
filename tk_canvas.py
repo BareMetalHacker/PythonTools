9b266e31e99111e07b84c87bf8cd78a6298f25da
Matty <79555484+BareMetalHacker@users.noreply.github.com>
2021-04-19 20:48:04 -0700

    Create Canvas and Graphics with TKINTER
    Create arcs and lines on canvas with TKINTER

---
+++ tk_canvas.py
@@ -0,0 +1,10 @@
+import tkinter
+from tkinter import *
+
+win = Tk()
+c = Canvas(win, height=250,width=300,bg='blue')
+coord=10,50,240,210
+arc = c.create_arc(coord,start=0,extent=150,fill='red')
+line = c.create_line(10,10,200,200 , fill='white')
+c.pack()
+win.mainloop()
