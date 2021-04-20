7b663012c1da1a6d52634c0f916fde22e7f2587f
Matty <79555484+BareMetalHacker@users.noreply.github.com>
2021-04-20 12:52:12 -0700

    Creating a menu bar with TKINTER
    Creating a menu bar with TKINTER

---
+++ tk_menu_bar.py
@@ -0,0 +1,41 @@
+import tkinter
+from tkinter import *
+
+win = Tk()
+
+def nothing():
+    file = Toplevel(win)
+    button = Button(file,text='do nothing')
+    button.pack()
+
+menubar = Menu(win)
+filemenu = Menu(menubar)
+
+filemenu.add_command(label='New Window' , command=nothing)
+filemenu.add_separator()
+filemenu.add_command(label='Open' , command=nothing)
+filemenu.add_command(label='Save' , command=nothing)
+filemenu.add_command(label='Save As ' , command=nothing)
+filemenu.add_command(label='Close' , command=nothing)
+filemenu.add_separator()
+filemenu.add_command(label='Exit' , command=win.quit)
+
+menubar.add_cascade(label='File' , menu = filemenu)
+
+editmenu = Menu(menubar)
+editmenu.add_command(label='Undo' , command=nothing)
+editmenu.add_command(label='Redo' , command=nothing)
+editmenu.add_separator()
+editmenu.add_command(label='Cut' , command=nothing)
+editmenu.add_command(label='Copy' , command=nothing)
+editmenu.add_command(label='Paste' , command=nothing)
+editmenu.add_separator()
+editmenu.add_command(label='Exit' , command=win.quit)
+
+menubar.add_cascade(label='Edit' , menu = editmenu)
+
+
+win.config(menu=menubar)
+
+
+win.mainloop()
