b8954d94614397060bea75f5cdf9b3077960c4f1
Matty <79555484+BareMetalHacker@users.noreply.github.com>
2021-04-26 07:30:16 -0700

    Just the standard dice roll project
    I had to look this one up when I was first asked to do it.  I think mine came from pythonforbeginners (just citing the source here).

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,8 +1,11 @@
 import random
+#imports random library
 min = 1
 max = 6
+#sets min and max of integers
 
 roll_again = "yes"
+#function 'rolls' the dice
 
 while roll_again == "yes" or roll_again == "y":
     print "Rolling the dices..."
